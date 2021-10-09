407f1c4a00a0ff5dd115bfc67f8d069c35d3f009
Yashashvi678 <81162244+Yashashvi678@users.noreply.github.com>
2021-10-09 18:36:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,100 @@ quick_draw_data_set=["aircraft carrier","airplane","alarm clock","ambulance","an
 random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
 console.log(quick_draw_data_set[random_number]);
 sketch = quick_draw_data_set[random_number];
-document.getElementById('sketch_to_be_drawn').innerHTML = 'Sketch To be Drawn: ' + sketch;
+document.getElementById('sketch_to_be_drawn').innerHTML = 'Sketch To be Drawn : ' + sketch;
 
 timer_counter = 0;
 timer_check = "";
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+
+
+function updateCanvas()
+{
+    background("white");
+    random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1); 
+    console.log(quick_draw_data_set[random_number]);
+    sketch = quick_draw_data_set[random_number];
+    document.getElementById('sketch_to_be_drawn').innerHTML = 'Sketch To be Drawn : ' + sketch;
+
+}
+
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+  }
+
+
+function preload()
+{
+  classifier = ml5.imageClassifier('DoodleNet');
+}
+
+
+  function draw()
+  {
+      check_sketch();
+
+      if(drawn_sketch == sketch)
+      {
+          answer_holder = "set";
+          score++;
+          document.getElementById("score_update").innerHTML = "score : " + score;
+      }
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+
+  }
+
+  function check_sketch()
+  {
+      timer_counter++;
+      document.getElementById("timer_update").innerHTML = 'Timer : ' + timer_counter;
+      console.log(timer_counter);
+
+      if(timer_counter > 500)
+      {
+          timer_counter = 0;
+          timer_check = "completed";
+      }
+
+      if(timer_check == "completed" || answer_holder == "set")
+      {
+          timer_check = "";
+          answer_holder = "";
+          updateCanvas();
+      }
+  }
+
+  function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+
+    console.log(results);
+    
+    drawn_sketch = results[0].label;
+
+    document.getElementById('label').innerHTML = 'Your Sketch : ' + results[0].label;
+    
+    document.getElementById('confidence').innerHTML = 'Confidence : ' + Math.round(results[0].confidence * 100) + '%';
+
+}
